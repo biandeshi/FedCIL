1de6b51839ace577ca2cffc26a11c1ea8b091a2e
biandeshi <1345424029@qq.com>
2023-11-30 14:13:38 +0800

    update

--- run-ours.sh
+++ run-ours.sh
@@ -1,8 +1,8 @@
 # FedCIL 
-CUDA_VISIBLE_DEVICES=0 python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cuda" --offline 0 --lr-CIFAR 0.0001 --naive 0 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 50
+CUDA_VISIBLE_DEVICES=-1 python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cpu" --offline 0 --lr-CIFAR 0.0001 --naive 0 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 50
 
 # FedCIL 
-CUDA_VISIBLE_DEVICES=0 python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cuda" --offline 0 --lr-CIFAR 0.0001 --naive 0 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 100
+# CUDA_VISIBLE_DEVICES=0 python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cuda" --offline 0 --lr-CIFAR 0.0001 --naive 0 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 100
 
 # ACGAN + FedAvg
-CUDA_VISIBLE_DEVICES=2 screen python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cuda" --offline 0 --lr-CIFAR 0.0001 --naive 1 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 0
+# CUDA_VISIBLE_DEVICES=2 screen python main.py --dataset Mnist-cl-5-EMnist-B --algorithm FedCL --batch_size 32 --gen_batch_size 32  --num_glob_iters 200 --local_epochs 400 --num_users 5 --times 3 --device "cuda" --offline 0 --lr-CIFAR 0.0001 --naive 1 --TASKS 5 --ft_iters=0 --fedavg=0 --md_iter 0
